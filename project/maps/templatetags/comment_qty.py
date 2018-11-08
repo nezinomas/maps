8243d1d98fd99c5991e9d994a3e25a4e6ecd8b25
Audrius <nezinomas@gmail.com>
2018-11-08 11:13:28 +0200

    TemplateTag comment_qty kuris patikrina ar dictionary egzistuoja post_id su qty

---
+++ project/maps/templatetags/comment_qty.py
@@ -0,0 +1,13 @@
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag(name='comment_qty')
+def comment_qty(dict, post_id):
+    qty = 0
+
+    if post_id in dict:
+        qty = dict[post_id]
+
+    return qty
