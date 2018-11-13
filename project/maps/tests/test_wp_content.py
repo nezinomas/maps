4fca408c2e3d8c21481ced7c60f55e0b6a604e96
Audrius <nezinomas@gmail.com>
2018-11-13 12:03:58 +0200

    Papildomi testai

--- project/maps/tests/test_wp_content.py
+++ project/maps/tests/test_wp_content.py
@@ -20,6 +20,18 @@ class WpContentTest(TestCase):
 
 
     def test_create_post_id_dictionary(self):
-            dict = wp.create_post_id_dictionary(self.trip)
+        dict = wp.create_post_id_dictionary(self.trip)
 
-            self.assertDictEqual(dict, {101: 0, 102: 0})
+        self.assertDictEqual(dict, {101: 0, 102: 0})
+
+
+    def test_comment_rest_link_01(self):
+        link = wp.create_comment_rest_link({101: 0, 102: 0})
+
+        self.assertEqual(link, 'comments?per_page=100&post=101&post=102')
+
+
+    def test_comment_rest_link_02(self):
+        link = wp.create_comment_rest_link({})
+
+        self.assertEqual(link, 'comments?per_page=100')
