8483c19eb6ac175a178087f1b9b1a3ae32653801
Audrius <nezinomas@gmail.com>
2022-07-09 17:28:48 +0300

    nsvrb

--- project/maps/urls.py
+++ project/maps/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls import static
 from django.urls import path
 
 from . import views
@@ -14,13 +12,3 @@ urlpatterns = [
     path('<slug:trip>/qty/', views.CommentQty.as_view(), name='comment_qty'),
     path('<slug:trip>/<slug:post>/comments/', views.Comments.as_view(), name='comments'),
 ]
-
-if settings.DEBUG:
-    from django.views.defaults import server_error, page_not_found, permission_denied
-
-    urlpatterns += [
-        path('403/', permission_denied, kwargs={'exception': Exception("Permission Denied")}, name='error403'),
-        path('404/', page_not_found, kwargs={'exception': Exception("Page not Found")}, name='error404'),
-        path('500/', server_error, name='error500'),
-    ]
-    urlpatterns += static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
