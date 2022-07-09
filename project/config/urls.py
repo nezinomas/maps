6d8474fd9ce1aa64bd5dbccbd2939f10c8a3b384
Audrius <nezinomas@gmail.com>
2022-07-09 14:29:44 +0300

    update urls

--- project/config/urls.py
+++ project/config/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 
 urlpatterns = [
@@ -8,9 +8,15 @@ urlpatterns = [
     path('', include('project.maps.urls')),
 ]
 
-# if settings.DEBUG:
-#     import debug_toolbar
+if settings.DEBUG:
+    import mimetypes
 
-#     urlpatterns += [
-#         path('__debug__/', include(debug_toolbar.urls)),
-#     ]
+    import debug_toolbar
+    from django.conf.urls.static import static
+
+    mimetypes.add_type("application/javascript", ".js", True)
+
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    urlpatterns = [path('__debug__/', include(debug_toolbar.urls)), ] + urlpatterns
