e428010fa733ad3cb87fced2fab2669a9d92f2ae
Audrius <nezinomas@gmail.com>
2022-07-14 21:45:18 +0300

    create folder in tracks

--- project/maps/tests/conftest.py
+++ project/maps/tests/conftest.py
@@ -8,10 +8,16 @@ from ..factories import UserFactory
 
 @pytest.fixture()
 def project_fs(fs):
-    fs.create_dir(path.join(settings.MEDIA_ROOT, 'tracks'))
+    fs.create_dir(path.join(settings.MEDIA_ROOT, 'tracks', '1'))
     fs.create_dir(path.join(settings.MEDIA_ROOT, 'points'))
 
-    template = path.join(settings.SITE_ROOT, 'maps', 'templates', 'maps', '0-points.js')
+    template = path.join(
+        settings.SITE_ROOT,
+        'maps',
+        'templates',
+        'maps',
+        '0-points.js'
+    )
     fs.create_file(template)
 
 
