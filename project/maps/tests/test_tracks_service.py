7555a522bcfa0c753e23b0b92d2d2d68eb051dcc
nezinomas <nezinomas@gmai.com>
2022-07-14 11:43:44 +0300

    TracksService final

--- project/maps/tests/test_tracks_service.py
+++ project/maps/tests/test_tracks_service.py
@@ -1,13 +1,41 @@
 import os
+from datetime import datetime, timezone
+from types import SimpleNamespace
 
 import pytest
 from django.conf import settings
-from mock import Mock, patch
+from mock import patch
 
-from ..factories import TripFactory
+from ..factories import TrackFactory, TripFactory
+from ..models import Statistic, Track
 from ..utils.tracks_service import TracksService
 
+TRACKS_SERVICE = 'project.maps.utils.tracks_service.TracksService'
 GET_TRIP = 'project.maps.utils.tracks_service.get_trip'
+
+pytestmark = pytest.mark.django_db
+
+
+@pytest.fixture
+def _activity():
+    activity = SimpleNamespace()
+
+    activity.activity_type = 'Biking'
+    activity.distance = 12345.6
+    activity.duration = 1918.1
+    activity.avg_speed = 23.40
+    activity.max_speed = 47.52
+    activity.calories = 33
+    activity.cadence_avg = 44.4
+    activity.hr_avg = 55.5
+    activity.hr_max = 200
+    activity.ascent = 111.0
+    activity.descent = 222.0
+    activity.altitude_min = 6.0
+    activity.altitude_max = 7.0
+    activity.start_time = datetime(2022, 1, 1, 3, 2, 1, tzinfo=timezone.utc)
+
+    return activity
 
 
 def test_tracks_service_init_with_trip():
@@ -32,6 +60,87 @@ def test_save_data_no_trip(mck):
     assert actual == 'No trip found'
 
 
+@patch(TRACKS_SERVICE + '.get_files')
+def test_save_data_no_tcx_files(mck_files):
+    mck_files.return_value = []
+
+    actual = TracksService(trip=TripFactory.build()).save_data()
+
+    assert actual == f'No tcx files in {settings.MEDIA_ROOT}/tracks'
+
+
+@patch(TRACKS_SERVICE + '.track_list_for_update')
+@patch(TRACKS_SERVICE + '.get_files')
+def test_save_data_no_tcx_files(mck_files, mck_tracks):
+    mck_files.return_value = ['x']
+    mck_tracks.return_value = None
+
+    actual = TracksService(trip=TripFactory.build()).save_data()
+
+    assert actual == 'All tracks are updated'
+
+
+@patch(TRACKS_SERVICE + '.get_data_from_tcx_file')
+@patch(TRACKS_SERVICE + '.track_list_for_update')
+@patch(TRACKS_SERVICE + '.get_files')
+def test_save_data_save_track_data(mck_files, mck_tracks, mck_data, _activity):
+    trip = TripFactory()
+    mck_files.return_value = ['x']
+    mck_tracks.return_value = ['y']
+    mck_data.return_value = _activity
+
+    TracksService(trip=trip).save_data()
+
+    actual = Track.objects.last()
+
+    assert actual.title == 'y'
+    assert actual.date == datetime(2022, 1, 1, 3, 2, 1, tzinfo=timezone.utc)
+    assert actual.activity_type == 'Biking'
+    assert actual.trip.title == 'Trip'
+
+
+@patch(TRACKS_SERVICE + '.get_data_from_tcx_file')
+@patch(TRACKS_SERVICE + '.track_list_for_update')
+@patch(TRACKS_SERVICE + '.get_files')
+def test_save_data_save_track_statistic_data(mck_files, mck_tracks, mck_data, _activity):
+    trip = TripFactory()
+    mck_files.return_value = ['x']
+    mck_tracks.return_value = ['y']
+    mck_data.return_value = _activity
+
+    TracksService(trip=trip).save_data()
+
+    actual = Statistic.objects.last()
+
+    assert round(actual.total_km, 2) == 12.35
+    assert actual.total_time_seconds == 1918.1
+    assert round(actual.avg_speed, 2) == 23.40
+    assert round(actual.max_speed, 2) == 47.52
+    assert actual.calories == 33.0
+    assert actual.avg_cadence == 44.4
+    assert actual.avg_heart == 55.5
+    assert actual.max_heart == 200
+    assert actual.avg_temperature == None
+    assert actual.ascent == 111.0
+    assert actual.descent == 222.0
+    assert actual.min_altitude == 6.0
+    assert actual.max_altitude == 7.0
+
+
+@patch(TRACKS_SERVICE + '.get_data_from_tcx_file')
+@patch(TRACKS_SERVICE + '.track_list_for_update')
+@patch(TRACKS_SERVICE + '.get_files')
+def test_save_data_save_success(mck_files, mck_tracks, mck_data, _activity):
+    trip = TripFactory()
+    mck_files.return_value = ['x']
+    mck_tracks.return_value = ['y']
+    mck_data.return_value = _activity
+
+    actual = TracksService(trip=trip).save_data()
+
+    assert actual == 'Successfully synced data from tcx files'
+
+
 def test_tcx_file_list(fs):
     directory = os.path.join(settings.MEDIA_ROOT, 'tracks')
     fs.create_file(os.path.join(directory, '1.tcx'))
@@ -42,3 +151,30 @@ def test_tcx_file_list(fs):
     actual = TracksService(trip=TripFactory.build()).get_files()
 
     assert actual == ['1', '2']
+
+
+def test_track_list_for_update():
+    track = TrackFactory()
+
+    files = ['999', '111']
+
+    actual = TracksService(trip=track.trip).track_list_for_update(files)
+
+    assert actual == ['111']
+
+
+@patch('project.maps.utils.tracks_service.TCXReader.read')
+def test_get_data_from_tcx_file(mck, fs, _activity):
+    fs.create_file(os.path.join(settings.MEDIA_ROOT, 'tracks', 'XXX.tcx'))
+
+    mck.return_value = _activity
+
+    actual = TracksService().get_data_from_tcx_file('XXX')
+
+    assert actual == _activity
+
+
+def test_get_data_from_tcx_file_no_file(fs):
+    actual = TracksService().get_data_from_tcx_file('XXX')
+
+    assert not actual
--- project/maps/utils/tracks_service.py
+++ project/maps/utils/tracks_service.py
@@ -1,8 +1,10 @@
 import os
 from typing import List
-from django.conf import settings
 
-from ..models import Trip
+from django.conf import settings
+from tcxreader.tcxreader import TCXReader
+from tcxreader.tcx_exercise import TCXExercise
+from ..models import Track, Trip, Statistic
 from ..utils.common import get_trip
 
 
@@ -13,6 +15,18 @@ class TracksService:
     def save_data(self) -> str:
         if not self.trip:
             return 'No trip found'
+
+        files = self.get_files()
+        if not files:
+            return f'No tcx files in {settings.MEDIA_ROOT}/tracks'
+
+        ids = self.track_list_for_update(files)
+        if not ids:
+            return 'All tracks are updated'
+
+        self.save_track_and_statistic(ids)
+
+        return 'Successfully synced data from tcx files'
 
     def get_files(self) -> List[str]:
         directory = os.path.join(settings.MEDIA_ROOT, 'tracks')
@@ -28,3 +42,56 @@ class TracksService:
             files.append(file[:-4])
 
         return files
+
+    def track_list_for_update(self, files: List[str]) -> List[Track]:
+        # get all tracks for current trip
+        tracks = \
+            Track.objects \
+            .filter(trip=self.trip) \
+            .values_list('title', flat=True)
+
+        # find tracks withount statistic
+        ids = list(set(files) - set(tracks))
+
+        return list(ids)
+
+    def get_data_from_tcx_file(self, file) -> TCXExercise:
+        file = os.path.join(settings.MEDIA_ROOT, 'tracks', f'{file}.tcx')
+
+        if not os.path.exists(file):
+            return
+
+        reader = TCXReader()
+        data = reader.read(file)
+
+        return data
+
+    def save_track_and_statistic(self, ids: List):
+        for id in ids:
+            activity = self.get_data_from_tcx_file(id)
+
+            # create track
+            track = Track.objects.create(
+                title=id,
+                date=activity.start_time,
+                activity_type=activity.activity_type,
+                trip=self.trip
+            )
+
+            # create track statistics
+            stats = {
+                'total_km': activity.distance / 1000,
+                'total_time_seconds': activity.duration,
+                'avg_speed': activity.avg_speed,
+                'max_speed': activity.max_speed,
+                'calories': activity.calories,
+                'avg_cadence': activity.cadence_avg,
+                'avg_heart': activity.hr_avg,
+                'max_heart': activity.hr_max,
+                'min_altitude': activity.altitude_min,
+                'max_altitude': activity.altitude_max,
+                'ascent': activity.ascent,
+                'descent': activity.descent,
+            }
+
+            Statistic.objects.create(track=track, **stats)
