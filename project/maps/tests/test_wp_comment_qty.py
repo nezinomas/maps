8f7211fef5c6781a894464477d7854bb178bebc2
nezinomas <nezinomas@gmai.com>
2022-10-07 12:42:26 +0300

    fixed error

--- project/maps/tests/test_wp_comment_qty.py
+++ project/maps/tests/test_wp_comment_qty.py
@@ -1,7 +1,36 @@
-from mock import patch
-from freezegun import freeze_time
+import json
+from types import SimpleNamespace
 
-from django.test import TestCase
+import pytest
+from mock import call, patch
 
-from ..models import Trip, CommentQty
-from ..utils import wp_comments_qty as C
+from ..utils import wp_comments_qty as CQ
+
+
+@pytest.fixture
+def trip():
+    return SimpleNamespace(blog='Blog_Url', blog_category=6)
+
+
+@patch('project.maps.utils.wp_content.get_content')
+@patch('project.maps.utils.wp_content.get_posts_ids')
+def test_count_comments(mck_ids, mck_content, trip):
+    mck_ids.return_value = [1, 2]
+    mck_content.return_value = SimpleNamespace(text=json.dumps([{'post': 1}, {'post': 1}]))
+
+    actual = CQ.count_comments(trip)
+
+    assert actual == {1: 2, 2: 0}
+
+
+@patch('project.maps.utils.wp_content.get_content')
+@patch('project.maps.utils.wp_content.get_posts_ids')
+def test_count_comments_link(mck_ids, mck_content, trip):
+    mck_ids.return_value = [1, 2]
+    mck_content.return_value = SimpleNamespace(text=json.dumps([{'post': 1}, {'post': 1}]))
+
+    CQ.count_comments(trip)
+
+    expected = [call('Blog_Url', 'comments?post=1,2&_fields=post')]
+
+    assert mck_content.call_args_list == expected
