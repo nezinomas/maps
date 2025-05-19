6fb3238b1ab9b37fa2594aca96221fc5a243d169
nezinomas <nezinomas@gmail.com>
2025-05-19 15:12:22 +0300

    use postgis

--- project/config/settings/base.py
+++ project/config/settings/base.py
@@ -89,6 +89,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.humanize",
+    "django.contrib.gis",
     "bulk_update_or_create",
     "project.maps",
 ]
