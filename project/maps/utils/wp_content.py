4a5e11b3679348584338b70540247b2d1c4f93b0
nezinomas <nezinomas@gmail.com>
2025-05-20 13:31:59 +0300

    using worpress api directly with requests.get

--- project/maps/utils/wp_content.py
+++ project/maps/utils/wp_content.py
@@ -1,25 +1,30 @@
 import json
 from typing import Dict
 
+import requests
 from django.conf import settings
+
 from wordpress import API
 
 
 def get_content(blog_url, link_end):
-    wpapi = API(
-        url=blog_url,
-        consumer_key=settings.ENV["CONSUMER_KEY"],
-        consumer_secret=settings.ENV["CONSUMER_SECRET"],
-        api="wp-json",
-        version="wp/v2",
-        wp_user=settings.ENV["WP_USER"],
-        wp_pass=settings.ENV["WP_PASS"],
-        oauth1a_3leg=True,
-        creds_store="",
-        callback=f"{blog_url}/oauth1_callback",
-    )
+    # wpapi = API(
+    #     url=blog_url,
+    #     consumer_key=settings.ENV["CONSUMER_KEY"],
+    #     consumer_secret=settings.ENV["CONSUMER_SECRET"],
+    #     api="wp-json",
+    #     version="wp/v2",
+    #     wp_user=settings.ENV["WP_USER"],
+    #     wp_pass=settings.ENV["WP_PASS"],
+    #     oauth1a_3leg=True,
+    #     creds_store="",
+    #     callback=f"{settings.ENV['WP_BLOG_URL']}/oauth1_callback",
+    # )
 
-    return wpapi.get(link_end)
+    # return wpapi.get(link_end)
+
+    link = f"{settings.ENV['WP_BLOG_URL']}/wp-json/wp/v2/{link_end}"
+    return requests.get(link)
 
 
 def get_json(blog_url: str, link: str) -> Dict:
@@ -30,6 +35,7 @@ def get_json(blog_url: str, link: str) -> Dict:
 def get_all_pages_content(trip, link):
     per_page = 100
     link = f"{link}&per_page={per_page}"
+
     response = get_content(trip.blog, link)
     content = json.loads(response.text)
     pages = int(response.headers["X-WP-TotalPages"])
