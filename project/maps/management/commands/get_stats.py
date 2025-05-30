0ce4713cf32cef461ab14e8658bfaf97c3fbc123
nezinomas <nezinomas@gmail.com>
2025-05-30 16:08:07 +0300

    get_stats command for cronjobs

---
+++ project/maps/management/commands/get_stats.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from django.core.management.base import BaseCommand, CommandError
+
+from ...utils.tracks_service import TracksService, TracksServiceData
+
+
+class Command(BaseCommand):
+    help = "Get bike activities from Garmin"
+
+    def handle(self, *args, **options):
+        try:
+            # get data from garmin connect
+            data = TracksServiceData()
+            service = TracksService(data)
+            service.create()
+        except Exception as e:
+            raise CommandError(f"Can't sync with Garmin - {e}") from e
+
+        self.stdout.write(
+            self.style.SUCCESS(f"{datetime.now()}: successfully get Garmin activities")
+        )
