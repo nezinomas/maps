a8e1e9ae969164e31bde16746eb36958de09533f
nezinomas <nezinomas@gmail.com>
2025-05-30 16:38:05 +0300

    small refactoring

--- project/maps/utils/parse_tcx_file.py
+++ project/maps/utils/parse_tcx_file.py
@@ -5,25 +5,31 @@ from django.contrib.gis.geos import LineString
 from lxml import etree
 
 
-def get_track_path(file_path: Path) -> LineString:
-    namespaces = {
-        "ns": "http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2",
-        "ns3": "http://www.garmin.com/xmlschemas/ActivityExtension/v2",
-    }
+NAMESPACES = {
+    "ns": "http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2",
+    "ns3": "http://www.garmin.com/xmlschemas/ActivityExtension/v2",
+}
 
+TRACKPOINT_TAG = (
+    "{http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2}Trackpoint"
+)
+
+LON = "ns:LongitudeDegrees/text()"
+LAT = "ns:LatitudeDegrees/text()"
+
+
+def get_track_path(file_path: Path) -> LineString:
     coordinates = []
     context = etree.iterparse(
         file_path,
-        tag="{http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2}Trackpoint",
+        tag=TRACKPOINT_TAG,
     )
-    _lon = "ns:LongitudeDegrees/text()"
-    _lat = "ns:LatitudeDegrees/text()"
     for _, elem in context:
-        pos = elem.xpath("ns:Position", namespaces=namespaces)
+        pos = elem.xpath("ns:Position", namespaces=NAMESPACES)
         if (
             pos
-            and (lon := pos[0].xpath(_lon, namespaces=namespaces))
-            and (lat := pos[0].xpath(_lat, namespaces=namespaces))
+            and (lon := pos[0].xpath(LON, namespaces=NAMESPACES))
+            and (lat := pos[0].xpath(LAT, namespaces=NAMESPACES))
         ):
             coordinates.append((round(float(lon[0]), 5), round(float(lat[0]), 5)))
         elem.clear()
@@ -40,12 +46,9 @@ def get_track_date(file_path: Path) -> datetime:
     tree = etree.parse(file_path)
     root = tree.getroot()
 
-    # Define the namespace for TCX
-    namespaces = {"ns": "http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2"}
-
     # Extract datetime from the Activity Id
-    activity = root.find(".//ns:Activity", namespaces)
-    activity_id = activity.find("ns:Id", namespaces)
+    activity = root.find(".//ns:Activity", NAMESPACES)
+    activity_id = activity.find("ns:Id", NAMESPACES)
     if activity_id is None:
         return datetime.now()
 
