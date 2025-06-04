80c1473232ba12f5491255ffcc827c46f94fecac
nezinomas <nezinomas@gmail.com>
2025-06-04 14:14:16 +0300

    reorganize urls

--- project/maps/urls.py
+++ project/maps/urls.py
@@ -6,20 +6,23 @@ app_name = "maps"
 
 urlpatterns = [
     path("", views.Trips.as_view(), name="trips"),
-    path("<slug:trip>/", views.Map.as_view(), name="index"),
-    path("<slug:trip>/posts/", views.Posts.as_view(), name="posts"),
-    path("<slug:trip>/utils/", views.TripUtils.as_view(), name="trip_utils"),
-    path("<slug:trip>/download_tcx/", views.DownloadTcx.as_view(), name="download_tcx"),
+    path("utils/", views.Utils.as_view(), name="utils_index"),
+    path("utils/login/", views.Login.as_view(), name="login"),
+    path("utils/logout/", views.Logout.as_view(), name="logout"),
+    path("utils/<slug:trip>/", views.TripUtils.as_view(), name="trip_utils"),
+    path("utils/download_tcx/<slug:trip>/", views.DownloadTcx.as_view(), name="download_tcx"),
     path(
-        "<slug:trip>/update_tracks/",
+        "utils/update_tracks/<slug:trip>/",
         views.SaveNewTracks.as_view(),
         name="update_tracks",
     ),
     path(
-        "<slug:trip>/update_all_tracks/",
+        "utils/update_all_tracks/<slug:trip>/",
         views.RewriteAllTracks.as_view(),
         name="update_all_tracks",
     ),
+    path("<slug:trip>/", views.Map.as_view(), name="index"),
+    path("<slug:trip>/posts/", views.Posts.as_view(), name="posts"),
     path("<slug:trip>/qty/", views.CommentQty.as_view(), name="comment_qty"),
     path(
         "<slug:trip>/<int:post_id>/comments/", views.Comments.as_view(), name="comments"
