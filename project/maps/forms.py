012f2a6b46c70f4edb2056d23b43b936811aff72
nezinomas <nezinomas@gmail.com>
2025-06-12 12:55:06 +0300

    rename form class

--- project/maps/forms.py
+++ project/maps/forms.py
@@ -39,7 +39,7 @@ class TripForm(forms.ModelForm):
         return cleaned
 
 
-class GetGarminDataByDateForm(forms.Form):
+class GetTcxByDateForm(forms.Form):
     start_date = forms.DateField(required=True)
     end_date = forms.DateField(required=True)
 
