fde0f1ab02b3c97f1e50eb0240d121ec0f4aa76b
nezinomas <nezinomas@gmail.com>
2026-04-30 10:43:25 +0300

    Update code for new garminconnect

--- project/maps/utils/garmin_service.py
+++ project/maps/utils/garmin_service.py
@@ -34,30 +34,14 @@ class GarminApi:
             tokenstore_path.parent.mkdir(parents=True, exist_ok=True)
 
         try:
-            garmin = Garmin()
-            garmin.login(str(tokenstore_path))
-            garmin.get_user_profile()
-            return garmin
-        except (GarminConnectAuthenticationError, Exception) as e:
-            print(f"Tokens expired or invalid ({e}). Cleaning up...")
-
-            try:
-                shutil.rmtree(tokenstore_path)
-                tokenstore_path.parent.mkdir(parents=True, exist_ok=True)
-            except Exception as cleanup_err:
-                print(f"Failed to delete old tokens: {cleanup_err}")
-
-        try:
             garmin = Garmin(
                 email=username,
                 password=password,
                 is_cn=False,
                 return_on_mfa=False,
             )
-            garmin.login()
+            garmin.login(str(tokenstore_path))
 
-            # Save tokens for future use
-            garmin.garth.dump(str(tokenstore_path))
             return garmin
 
         except (
