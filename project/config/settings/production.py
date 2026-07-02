0617814a4842a079a0c47abbc3de74eb218615a5
nezinomas <nezinomas@gmail.com>
2026-07-02 12:32:55 +0300

    Changed default email address

--- project/config/settings/production.py
+++ project/config/settings/production.py
@@ -44,7 +44,7 @@ EMAIL_PORT = 25
 EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
 EMAIL_USE_TLS = False
-DEFAULT_FROM_EMAIL = "Invite <invite@bookkeeping.com>"
+DEFAULT_FROM_EMAIL = "Maps <maps@unknownbug.com>"
 
 
 LOGGING = {
